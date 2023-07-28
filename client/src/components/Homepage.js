767328a7ff640fd6ec3a032ad7ae1af13827dd13
Curtis Webb <curtwebb1194@@gmail.com>
2023-07-28 00:51:03 -0500

    Changed Website Layout and Style

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,75 +3,29 @@ import './Homepage.css';
 import "firebase/auth";
 
 export default function Homepage() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [showSearchBar, setShowSearchBar] = useState(false);
 
-
-
-    return (
-        <div>
-            <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Small Business - Start Bootstrap Template</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="..." /></div>
-                <div class="col-lg-5">
-                    <h1 class="font-weight-light">Unlock Efficiency <br></br>Unlock Creativity</h1>
-                    <p>Unlock the true potential of your coding prowess by harnessing the collective knowledge of our vibrant community. Share your genius code solutions, learn from others, and collaborate on exciting projects to take your skills to new heights!</p>
-                    <a class="btn btn-primary" href="/about">Learn More!</a>
-                </div>
-            </div>
-            <div class="card text-white bg-secondary my-5 py-4 text-center">
-                <div class="card-body"><p class="text-white m-0">"Streamline Your Code, Unleash Your Creativity!"
-
-
-
-
-</p></div>
-            </div>
-            <div class="row gx-4 gx-lg-5">
-                <div class="col-md-4 mb-5">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Search Snippets</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                        </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-5">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Snippet Manager</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
-                        </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-5">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Create A Snippet</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                        </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="Homepage">
+      <section>
+        <div className="MainHeader">
+          Organizing and Access Reusable Code
         </div>
-        <footer class="py-5 bg-dark">
-            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
-        </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
+        <div className="SecHeader">
+          a user-friendly interface to store and manage code snippets effectively
         </div>
-    );
+        <button onClick={() => setShowSearchBar(true)} className="search-button">
+          Search for Code Now
+        </button>
+      </section>
+
+      <section>
+        {showSearchBar && (
+          <div className="search-bar">
+            <input type="text" placeholder="Enter search query..." />
+          </div>
+        )}
+      </section>
+    </div>
+  );
 }
