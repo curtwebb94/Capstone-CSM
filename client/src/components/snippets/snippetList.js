3b3aa0391717693fc471e4bb2a038a53bf902112
Curtis Webb <curtwebb1194@@gmail.com>
2023-09-11 10:54:49 -0400

    small search bar  changes

--- client/src/components/snippets/snippetList.js
+++ client/src/components/snippets/snippetList.js
@@ -26,7 +26,7 @@ export default function SnippetList() {
       setPlaceholderIndex((prevIndex) =>
         prevIndex === placeholderOptions.length - 1 ? 0 : prevIndex + 1
       );
-    }, 2000); // Rotate every 2 seconds
+    }, 3000); // Rotate every 3 seconds
 
     return () => clearInterval(interval);
   }, []);
